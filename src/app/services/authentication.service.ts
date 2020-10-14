4a6b5314579e31e3fd11aaf74213e540ac055082
girishsrivatsa <girishsrivatsa@cse.iitb.ac.in>
2020-10-14 13:13:24 +0530

    added login,logout,home

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Login} from '../_models/login';
 import {UserReal} from '../_models/user-real';
-const LOGIN_URL = 'http://127.0.0.1:8000/api/auth/login/';
+const LOGIN_URL = 'https://back-dashboard.herokuapp.com/api/auth/login/';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
--- src/app/services/logout.service.ts
+++ src/app/services/logout.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const LOGOUT_URL = 'https://127.0.0.1:8000/api/auth/logout/';
+const LOGOUT_URL = 'https://back-dashboard.herokuapp.com/api/auth/logout/';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
