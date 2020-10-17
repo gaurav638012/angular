494f31c30b166272d8333491ac50b320d59b731f
girishsrivatsa <girishsrivatsa@cse.iitb.ac.in>
2020-10-17 17:44:16 +0530

    endless loop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    if(!this.authenticationService.currentUserValue)
+    if(localStorage.getItem('user')!=null&&localStorage.getItem('user')!='undefined')
     {
       this.router.navigate(['/login']);
     }
