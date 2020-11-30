925ecd5363d93e3c93ca195db08b2182b37b4352
girishsrivatsa <girishsrivatsa@cse.iitb.ac.in>
2020-11-30 21:18:09 +0530

    log out

--- src/app/services/add-course.service.ts
+++ src/app/services/add-course.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Course} from '../_models/course';
-import {Course} from '../_models/course';
 import {BASE_URL} from './base';
 const COURSE_URL = BASE_URL+'/api/courses/';
 const httpOptions = {
